af15999db5995bac8976b3e8d112fb10ba2e5af7
Marlon Veiga <marlon_veiga@hotmail.com>
2021-09-20 19:07:14 -0300

    Refactor legacy routes.js file and update index.html.

--- routes/legacy_routes.js
+++ routes/legacy_routes.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const {
+  randomJoke,
+  randomTen,
+  jokeByType,
+} = require("../handler");
 const LimitingMiddleware = require('limiting-middleware');
 
 
